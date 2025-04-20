fa29cd1aea0dc616709c72d80c3d7b1f28a61db1
花果山大圣 <316783812@qq.com>
2025-04-20 10:16:35 +0100

    Update play.js

--- play.js
+++ play.js
@@ -8,7 +8,7 @@ function playMp3(text) {
   audio = new Audio(url)
   audio.play()
 }
-// playMp3('hello interview')
+// playMp3('hello google')
 
 // 为所有带有 data-relingo-block 属性的元素添加播放按钮
 function addPlayButtonsToRelingoBlocks() {
@@ -25,13 +25,18 @@ function addPlayButtonsToRelingoBlocks() {
     playButton.style.cursor = 'pointer'
 
     // 添加点击事件监听器
-    playButton.addEventListener('click', () => {
+    playButton.addEventListener('click', (event) => {
       // 获取元素的文本内容
       const text = block.innerText.trim().slice(2)
       if (text) {
         // 调用 playMp3 函数播放文本
         playMp3(text)
       }
+      // 阻止事件冒泡
+      event.stopPropagation()
+
+      // 阻止默认事件
+      event.preventDefault()
     })
 
     // 将按钮插入到元素之前
@@ -50,3 +55,7 @@ addPlayButtonsToRelingoBlocks()
 // }
 
 // getVioceUrl('hello interview')
+
+// 生成一个链接地址，可以添加到收藏夹，点击后异步加载这个 js 文件
+
+// javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//cdn.jsdelivr.net/gh/shengxinjing/it-roadmap/play.js';void(0);
